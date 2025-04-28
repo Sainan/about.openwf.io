629f6c0e0babbdf2eea1519c48b70d7b122c2185
Sainan <63328889+Sainan@users.noreply.github.com>
2025-04-29 01:51:42 +0200

    Fix navbar dropdown active indicator for import & old versions page

--- _components/navbar.php
+++ _components/navbar.php
@@ -11,7 +11,7 @@ $path = strtok($_SERVER["REQUEST_URI"], '?');
 		<div class="collapse navbar-collapse" id="navbar-content">
 			<ul class="navbar-nav me-auto mb-0">
 				<li class="nav-item dropdown">
-					<a class="nav-link dropdown-toggle<?=((substr($path, 0, 6) == "/guide" || substr($path, 0, 25) == "/asset-replacements-guide") ? " active" : ""); ?>" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Guides</a>
+					<a class="nav-link dropdown-toggle<?=((substr($path, 0, 6) == "/guide" || substr($path, 0, 7) == "/import" || substr($path, 0, 25) == "/asset-replacements-guide" || substr($path, 0, 13) == "/old-versions") ? " active" : ""); ?>" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Guides</a>
 					<ul class="dropdown-menu">
 						<li><a class="dropdown-item<?=(substr($path, 0, 6) == "/guide" ? " active" : ""); ?>" href="/guide<?=$ext;?>">Setup</a></li>
 						<li><a class="dropdown-item<?=(substr($path, 0, 7) == "/import" ? " active" : ""); ?>" href="/import<?=$ext;?>">Importing</a></li>
