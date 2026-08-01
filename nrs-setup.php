21ef851fcac2fc0380d364e70ba0c5652d8b21df
Sainan <63328889+Sainan@users.noreply.github.com>
2026-08-01 09:24:04 +0200

    Update NRS setup to include lan-nrs

--- nrs-setup.php
+++ nrs-setup.php
@@ -9,19 +9,21 @@
 <body data-bs-theme="dark">
 	<?php require "_components/navbar.php"; ?>
 	<div class="container mt-3">
-		<p><abbr title="NAT Relay Server">NRS</abbr> is used to exchange presence and session information, which is needed to establish squads. Unlike other server infrastructure, there is currently no self-hostable NRS server due to abuse concerns. However, several NRS reimplementations are hosted publicly.</p>
+		<p><abbr title="NAT Relay Server">NRS</abbr> is used to exchange presence and session information, which is needed to establish squads. Because we don't want people spinning up "private servers" in the sense of being an alternative to the MMO experience that is offered on the live servers (especially where the server operator could add monetization), all options here have some trade-offs.</p>
 		<h3>e-nrs</h3>
-		<p>This instance supports 7.3.0 - 42.0.11 and <abbr title="Proxying allows people behind strict NAT to establish indirect connections to each other.">proxying</abbr>. You can configure SpaceNinjaServer to instruct your clients to connect to it like so:</p>
+		<p>This instance runs an implementation supporting 7.3.0 - 42.0.11 and <abbr title="Proxying allows people behind strict NAT to establish indirect connections to each other.">proxying</abbr>. You can configure SpaceNinjaServer to instruct your clients to connect to it like so:</p>
 		<ul>
 			<li>Set <code>"nrsAddresses"</code> to <code>["e-nrs.source.wf"]</code></li>
 			<li>Set <code>"dtls"</code> to <code>99</code></li>
 		</ul>
-		<h3>o-nrs</h3>
-		<p>This instance supports 35.5.0 - 40.0.5.1. You can configure SpaceNinjaServer to instruct your clients to connect to it like so:</p>
+		<h3>o-nrs</h3> <!-- will be discontinued in Jan 2027 -->
+		<p>This instance runs an implementation supporting 35.5.0 - 40.0.5.1. You can configure SpaceNinjaServer to instruct your clients to connect to it like so:</p>
 		<ul>
 			<li>Set <code>"nrsAddresses"</code> to <code>["o-nrs.source.wf"]</code></li>
 			<li>Set <code>"dtls"</code> to <code>99</code></li>
 		</ul>
+		<h3>lan-nrs</h3>
+		<p>This implementation supports 7.3.0 - 10.8.0 for up to 4 players in the same LAN. The latest release can be downloaded <a href="https://openwf.io/supplementals/lan-nrs-1.0.0.zip" download>here</a>. Take note of the README and on-screen instructions.</p>
 
 		<div class="row g-3 my-4">
 			<div class="col-6">
